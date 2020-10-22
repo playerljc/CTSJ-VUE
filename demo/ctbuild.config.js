7a0d820ba478aeb998b27b875cf628fc48fe1c22
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-10-22 20:22:53 +0800

    加入example和demo目录

--- demo/ctbuild.config.js
+++ demo/ctbuild.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   getConfig({ webpackConfig }) {
-    webpackConfig.module.rules[0].use[2].query.plugins.push([
-      '@babel/plugin-transform-modules-commonjs',
-      { strictMode: false },
-    ]);
+
   },
 };
