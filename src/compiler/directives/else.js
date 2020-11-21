bdec1f63563877a439878713908172bc05298791
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-11-21 17:42:04 +0800

    诸多问题的修改 1.v-for中的click事件 2.class和style样式的混入 3.执行表达式的策略

---
+++ src/compiler/directives/else.js
@@ -0,0 +1,126 @@
+import { hasVAttr, getVAttrNames } from './util';
+import { DIRECT_PREFIX, GROUP_KEY_NAME } from '../../shared/constants';
+import { execExpression, isElementNode, isTextNode } from '../../shared/util';
+
+/**
+ * hasVElse - 是否有v-else属性
+ * @param attrNames - Array 所有的指令属性集合
+ * @return {boolean}
+ */
+export function hasVElse(attrNames) {
+  return hasVAttr(attrNames, `${DIRECT_PREFIX}else`);
+}
+
+/**
+ * parseVElse
+ * @param context - Object 上下文对象
+ * @param el - HtmlElement 当前元素
+ * @param parentElement = HtmlElement 父元素
+ * @return {
+ *   valid : boolean 链路是否有效
+ *   result: boolean else的值
+ * }
+ */
+export function parseVElse({ context, el, parentElement }) {
+  // 说明el是v-for的一个克隆元素
+  if (!parentElement) {
+    // 寻找el元素的克隆元素
+    let groupName = el.getAttribute(GROUP_KEY_NAME);
+    // 获取含有GROUP_KEY_NAME属性且值等于groupName的元素
+    const srcEl = this.templateEl.querySelector(`[${GROUP_KEY_NAME}=${groupName}]`);
+    if (srcEl) {
+      parentElement = srcEl.parentElement;
+    }
+  }
+
+  if (!parentElement) {
+    return {
+      valid: false,
+      result: false,
+    };
+  }
+
+  // 获取parentElement中的所有childNodes
+  let childNodes;
+  if (parentElement.nodeName.toLowerCase() === 'template') {
+    childNodes = parentElement.content.childNodes;
+  } else {
+    childNodes = parentElement.childNodes;
+  }
+
+  // 获取el在childNodes中的位置
+  let elIndex = -1;
+  for (let i = 0, len = childNodes.length; i < len; i++) {
+    const childNode = childNodes.item(i);
+    if (childNode === el) {
+      elIndex = i;
+      break;
+    }
+  }
+
+  if (elIndex === -1) {
+    return {
+      valid: false,
+      result: false,
+    };
+  }
+
+  // 获取elIndex之前第一个v-if的位置
+  let firstIfIndex = -1;
+  for (let i = elIndex - 1; i >= 0; i--) {
+    const childNode = childNodes[i];
+    // 如果是元素 && 有v-if属性
+    if (isElementNode(childNode) && hasVAttr(getVAttrNames(childNode), `${DIRECT_PREFIX}if`)) {
+      firstIfIndex = i;
+      break;
+    }
+  }
+
+  if (firstIfIndex === -1) {
+    return {
+      valid: false,
+      result: false,
+    };
+  }
+
+  // v-if和v-else之前只能包含文本节点或者v-else-if
+  let valid = true;
+  const values = [];
+  for (let i = firstIfIndex + 1; i <= elIndex - 1; i++) {
+    const childNode = childNodes[i];
+
+    // 如果是文本节点
+    if (isTextNode(childNode)) {
+      // 如果文本节点不是''
+      if (childNode.nodeValue.trim() !== '') {
+        valid = false;
+        break;
+      }
+    } else {
+      const hasVElseIf = hasVAttr(getVAttrNames(childNode), `${DIRECT_PREFIX}else-if`);
+
+      // 如果不是v-else-if
+      if (!hasVElseIf) {
+        valid = false;
+        break;
+      }
+      // 如果是v-else-if
+      else {
+        values.push(
+          execExpression.call(this, context, childNode.getAttribute(`${DIRECT_PREFIX}else-if`)),
+        );
+      }
+    }
+  }
+
+  if (valid) {
+    // 计算v-if的值
+    const ifNode = childNodes[firstIfIndex];
+    values.push(execExpression.call(this, context, ifNode.getAttribute(`${DIRECT_PREFIX}if`)));
+  }
+
+  return {
+    valid,
+    result: valid ? !values.some((value) => value) : false,
+  };
+}
